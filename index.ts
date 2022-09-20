793f30d93c6cf6a207b6a397375f52b198e4717b
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2022-09-20 15:28:38 +0800

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-import fsx from 'fs-extra'
-
-import { OpenApiReader } from './process/types'
 
 const mode = process.argv[2]
 const main = async() => {
@@ -11,18 +8,6 @@ const main = async() => {
         app.listen('9999', () => {
             console.log('running: http://localhost:9999')
         })
-    }
-    if (mode === 'types') {
-        if (fsx.existsSync('types')) {
-            fsx.rmSync('types', { recursive: true })
-        }
-        fsx.mkdirSync('types')
-        const files = fsx.readdirSync('./docs')
-        for (let file of files) {
-            const reader = OpenApiReader.read(`./docs/${file}`)
-            const content = await reader.exportNextgenRequest()
-            fsx.writeFileSync(`./types/${file.slice(0, -5)}.ts`, content)
-        }
     }
 }
 
